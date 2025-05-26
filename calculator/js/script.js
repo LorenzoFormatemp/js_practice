6f4f08228b1db474009dc117da000ff49b7ea918
LorenzoFormatemp <lorenzotesoro20@gmail.com>
2025-05-26 15:31:59 +0200

    add operation

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -9,19 +9,21 @@
 function calculate(operation){    
     const n1 = parseFloat(document.getElementById("num1").value);
     const n2 = parseFloat(document.getElementById("num2").value);
-    
+    let response;
+
     // controllo che i due valori siano effettivamente dei numeri
     if(isNaN(n1) || isNaN(n2)){
         console.log('Input non validi! Gli input inseriti devono essere numeri');
         return;
     }
 
+    // serie di condizioni
+    switch (operation) {
+        case '+': response = n1 + n2; break;
+        case '-': response = n1 - n2; break;
+        case '*': response = n1 * n2; break;
+        case '/': response = n2 !== 0 ? n1 / n2 : 'Non puoi dividere per zero'; break;
+    }
 
-
-
-
-
-
-
-
+    document.getElementById("result").innerText = `Il risultato è: ${response}`;
 }
