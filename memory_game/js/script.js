30cc11da5bc467c7f685ad29c98c956d6a03c28f
LorenzoFormatemp <lorenzotesoro20@gmail.com>
2025-05-27 17:31:13 +0200

    add flip function completed

--- memory_game/js/script.js
+++ memory_game/js/script.js
@@ -9,6 +9,8 @@ const symbols = ["😀","🎉","🚀","🌟","🍕","🍔","🍎","🍓","😀",
 let moves = 0;
 let matches = 0;
 let lockBoard = false;
+let first = null;
+let second = null;
 
 // restituire l'array di partenza modificata => la stessa array con i simboli in posizione diversa
 function shuffle(arr){
@@ -20,12 +22,10 @@ function shuffle(arr){
     }
 }
 
-
 // lancio al caricamento
 function setup(){
     // mischio l'array
     shuffle(symbols);
-    console.log(symbols);
     const board = document.getElementById('gameBoard');
     
     /* 
@@ -41,25 +41,71 @@ function setup(){
     `; // mostro a schermo che il gioco è ricominciato
     lockBoard = false; // sblocco la scacchiera
 
-    // genero le card
+    //ciclo i simboli
     symbols.forEach((sym, index) => {
-        const card = document.createElement('div');
-        card.className = 'card';
-        card.dataset.symbol = sym;
-        card.dataset.index = index;
-        
-        // TODO: aggiungere alla card la funzione flip
-        card.onclick = () => flip(card);
-        
-        board.appendChild(card);
+        const card = document.createElement('div'); // creo l'elemento
+        card.className = 'card'; // gli aggiungo la classe card
+        card.dataset.symbol = sym; // gli setto un attributo data-symbol
+        card.dataset.index = index; // gli setto un attributo data-index
+        card.onclick = () => flip(card); // aggiungo la funzione di flip all'onclick sulla card
+        board.appendChild(card); // appendo la card nel dom
     });
 }
 
 
-function flip(card){}
+function flip(card){
+
+    if(lockBoard || card.classList.contains("flipped")){
+        return;
+    }
+    
+    card.innerText = card.dataset.symbol; // sfrutto il data-symbol per metterglielo come simbolo quando flippo
+    card.classList.add("flipped"); // aggiungo questa classe per modificare il colore
+
+    if(!first){ // così mi assicuro di avere sempre una prima card da confrontare
+        first = card;
+        return;
+    }
+
+    second = card;
+    lockBoard = true;
+    moves++;
+    document.getElementById("moves").innerText = moves;
+
+    if(first.dataset.symbol === second.dataset.symbol){ // entra solo se c'è il match
+        matches++; // incremento i match
+        document.getElementById("matches").innerText = matches;// scrivo dentro ai match il nuovo valore
+        document.getElementById('feedback').innerHTML = `
+            <div class="alert alert-success">Congratulations! It's a match</div>
+        `;//informo l'utente
+        resetSelection();
+        
+        if(matches === (symbols.length / 2)){
+            document.getElementById('feedback').innerHTML = `<div class="alert alert-success">Congratulations! You completed the game in ${moves} moves.</div>`
+        } 
 
 
 
+    } else { // entra quando non c'è il match
+        document.getElementById('feedback').innerHTML = `<div class="alert alert-danger">No match! Try again</div>`;
+
+        setTimeout(() => {
+            first.innerText = '';
+            second.innerText = '';
+            first.classList.remove("flipped");
+            second.classList.remove("flipped");
+            resetSelection();
+        }, 1000);
+    }
+
+}
+
+function resetSelection(){
+    /* first = null;
+    second = null; */
+    [first, second] = [null, null];
+    lockBoard = false;
+}
 
 setup();
 
